35f2d3eda87978dad677ecca7b1bd3a6aae4c6a6
Michał Nerwiński <42096347+n3rwus@users.noreply.github.com>
2022-06-23 21:21:00 +0200

    Script generating db

--- artefakty_tab/sql_scripts/category.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Category] (
-    [Id]           INT            IDENTITY (1, 1) NOT NULL,
-    [CategoryName] NVARCHAR (200) NULL, 
-    CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
-);
--- artefakty_tab/sql_scripts/folder.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE TABLE [dbo].[Folder] (
-    [Id]                INT             IDENTITY (1, 1) NOT NULL,
-    CONSTRAINT [PK_Folder] PRIMARY KEY ([Id])
-);
