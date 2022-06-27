a81c15db696085eb90a9401c3c931d6f0331cf32
Michał Nerwiński <42096347+n3rwus@users.noreply.github.com>
2022-06-27 13:01:02 +0200

    creating models for images

---
+++ Models/Images/AddImagesRequest.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAlbum.Models.Images
+{
+    /*    AddImages
+    --------------------
+    Request:
+
+    UserToken: string,
+    Title: string,
+    folderId: Id(int?), 
+    Description: string,
+    Tags: string[],
+    Date: date,
+    Images: array[bytearray[]]
+
+    Response:
+
+    Status(np. 200 jeśli uda się dodać)*/
+    public class AddImagesRequest
+    {
+        [Required]
+        public string Token { get; set; }
+        public int? FolderId { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Description { get; set; }
+        public List<string>? Categores { get; set; }
+        [Required]
+        public List<byte[]> Images { get; set;}
+
+    }
+}
---
+++ Models/Images/DeleteImagesRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace WebAlbum.Models.Images
+{
+/*    DeleteImage
+-----------------
+Request:
+
+UserToken: string
+idZdjęcia: int/string
+
+Response:
+
+Status*/
+    public class DeleteImagesRequest
+    {
+    }
+}
